a526b8374cfc9600bcc31c60dec71d1d9ee8815f
Сокова Анастасия <99127518+AnastasiaSokova@users.noreply.github.com>
2022-03-02 10:41:18 +0300

    Сокова Анастасия 21-ИСТ-3

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,6 +1,19 @@
 // Copyright 2021 NNTU-CS
 
 int cbinsearch(int *arr, int size, int value) {
-  //  поместить сюда реализацию алгоритма
+    int i = 0, j = size - 1;
+    int k = 0;
+    while (i < j) {
+        int mid = i + (j - i) / 2;
+        if (arr[mid] >= value) {
+            j = mid;
+        } else {
+            i = mid + 1;
+        }
+        if (arr[j] == value) {
+            k++;
+        }
+    }
+    return k;
   return 0; // если ничего не найдено
 }
